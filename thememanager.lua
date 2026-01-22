ffcf04004c8c9e24c48745b73c099588f248dac4
toastd <toastdtoastd@gmail.com>
2026-01-22 04:03:33 -0800

    Update thememanager.lua

--- thememanager.lua
+++ thememanager.lua
@@ -223,7 +223,7 @@ local ThemeManager = {} do
 
 	function ThemeManager:CreateGroupBox(tab)
 		assert(self.Library, 'Must set ThemeManager.Library first!')
-		return tab:AddLeftGroupbox('Themes')
+		return tab:AddRightGroupbox('Themes')
 	end
 
 	function ThemeManager:ApplyToTab(tab)
